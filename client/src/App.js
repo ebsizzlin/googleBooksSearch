c9490a28814d944cc00e982ff1ea08e8af9071ad
Ella Thompson <fang98t@gmail.com>
2021-02-10 18:33:04 -0500

    altering src app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,26 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React from "react";
+// import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+import Nav from "./components/Nav";
+import NoMatch from "./pages/NoMatch";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Search}/>
+          <Route exact path="/search" component={Search}/>
+          <Route exact path="/saved" component={Saved}/>
+          <Route component={NoMatch} />
+        </Switch>
       </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    </Router>
   );
 }
-
 
 export default App;
